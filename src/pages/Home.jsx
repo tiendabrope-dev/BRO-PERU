9bd015fff3e71d5ac7d6d3f02ffe2100a56eafb5
tiendabrope-dev <tiendabro.pe@gmail.com>
2026-08-23 01:55:15 -0500

    Solucionar imagen de hero cuadro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero';
 import Categorias from '../components/Categorias';
 import BestSellers from '../components/BestSellers';
 
-import heroCuadro from '../assets/hero/hero-cuadro.jpg';
+import heroCuadro from '../assets/hero/hero-cuadro.png';
 
 function Home({
   categorias,
